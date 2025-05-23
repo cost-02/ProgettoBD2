04440808e6e5ac56284a18ca07f4f87422c1c5df
andre <a.salurso3@studenti.unisa.it>
2025-05-23 11:59:51 +0200

    Visualizzazione delle recensioni nella Join

--- car-review-project/app.py
+++ car-review-project/app.py
@@ -23,5 +23,19 @@ def show_reviews():
     reviews = list(reviews_col.find())
     return render_template("reviews.html", reviews=reviews)
 
+@app.route('/join')
+def show_join():
+    joined_reviews = list(reviews_col.aggregate([
+    {
+        "$lookup": {
+        "from": "cars",
+        "localField": "car_id", # presente nella recensione
+        "foreignField": "id", # campo 'id' presente nell'auto
+        "as": "car_info"
+         }
+    }
+    ]))
+    return render_template("join.html", reviews=joined_reviews)
+
 if __name__ == "__main__":
     app.run(debug=True)
